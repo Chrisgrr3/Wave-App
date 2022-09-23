824875d32d44681e2674b427b66a972613a2c75c
Christopher Guerrero <chrisgrr3@gmail.com>
2022-09-22 18:39:38 -0700

    Update hardhat.config.js
    Created the accounts task so that we can view the addresses hardhat gives us to simulate real users

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,17 @@
+const { task } = require("hardhat/config");
+
 require("@nomicfoundation/hardhat-toolbox");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
